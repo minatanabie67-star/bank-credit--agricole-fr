8ede3a3956556ea61a2002256ce0a2414efd4317
minatanabie67-star <minatanabie67@gmail.com>
2026-03-29 07:40:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# 🔥 toujours revenir à la connexion
 @app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+# page connexion
+@app.route('/login')
 def login():
     return render_template('connexion.html')
 
+# traitement du formulaire
 @app.route('/login', methods=['POST'])
 def connect():
-    user = request.form.get("identifiant")
-    password = request.form.get("motdepasse")
+    user = request.form.get('identifiant')
+    password = request.form.get('motdepasse')
 
     if user == "CA84739261" and password == "1615":
-        return redirect('/dashboard')
+        return render_template('tableau.html')
     else:
         return "❌ Identifiant ou mot de passe incorrect"
-   
 
+# sécurité : bloquer accès direct dashboard
 @app.route('/dashboard')
 def dashboard():
-    return render_template('tableau.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return redirect(url_for('login'))
