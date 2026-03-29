503464a8e395c23e41d61d7eca0347a7640ca9f2
minatanabie67-star <minatanabie67@gmail.com>
2026-03-29 08:15:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 # toujours revenir à la connexion
 @app.route('/')
 def home():
-    return redirect(url_for('connexion.html'))
+    return redirect(url_for('login.html'))
 
 # page connexion
 @app.route('/login')
 def login():
-    return render_template('connexion.html')
+    return render_template('login.html')
 
 # traitement du formulaire
 @app.route('/login', methods=['POST'])
@@ -19,11 +19,11 @@ def connect():
     password = request.form.get('motdepasse')
 
     if user == "CA84739261" and password == "1615":
-        return render_template('tableau.html')
+        return render_template('dashboard.html')
     else:
         return "❌ Identifiant ou mot de passe incorrect"
 
 # sécurité : empêcher accès direct au dashboard
 @app.route('/dashboard')
 def dashboard():
-    return redirect(url_for('tableau.hmtl'))
+    return redirect(url_for('dashboard.html'))
